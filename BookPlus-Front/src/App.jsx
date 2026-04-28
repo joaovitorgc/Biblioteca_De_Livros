e512de3f56b74e268ee368cbf074500ed45780ba
joaovitorgc <victordeoliveira48283@gmail.com>
2026-04-28 08:33:05 -0300

    Feat: Login
    Criação da Página de Login.

--- BookPlus-Front/src/App.jsx
+++ BookPlus-Front/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header.jsx";
 import Home from "./pages/Home.jsx";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Footer from "./components/Footer/Footer.jsx";
+import Login from "./pages/Login/Login.jsx";
 function App() {
 
 
@@ -12,6 +13,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={ <Home/> } />
+                <Route path="/Login" element={ <Login/> } />
             </Routes>
             <Footer/>
 
---
+++ BookPlus-Front/src/pages/Login/Login.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import estilos from './Login.module.css';
+
+export default function Login(){
+    return (
+        <main className={estilos.containerPrincipal}>
+            <section className={estilos.painelEsquerdo}>
+                <div className={estilos.cartaoLogin}>
+                    <h2 className={estilos.titulo}>Realize o Login</h2>
+
+                    <form className={estilos.formulario}>
+                        <div className={estilos.grupoEntrada}>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" />
+                        </div>
+
+                        <div className={estilos.grupoEntrada}>
+                            <label htmlFor="senha">Senha</label>
+                            <input type="password" id="senha" />
+                        </div>
+
+                        <div className={estilos.esqueceuSenha}>
+                            <span>Esqueci minha Senha? </span>
+                            <a href="#redefinir">Redefinir</a>
+                        </div>
+
+                        <button type="submit" className={estilos.botaoEntrar}>
+                            ENTRAR
+                        </button>
+                    </form>
+
+                    <div className={estilos.secaoCadastro}>
+                        <p>Ainda não possui Cadastro?</p>
+                        <button type="button" className={estilos.botaoCadastrar}>
+                            CADASTRE-SE
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section className={estilos.painelDireito}>
+                <img
+                    src="/oRetratoDeDorianGray-livro.jpg"
+                    alt="Capa do livro O Retrato de Dorian Gray"
+                    className={estilos.imagemLivro}
+                />
+            </section>
+        </main>
+    );
+};
+
